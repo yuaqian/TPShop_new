943ee093d598d1408b6db4311dc0bd6fdd02eefd
DESKTOP-PJQETOT\14326 <1119367693@qq.com>
2018-08-24 20:38:50 +0800

    初始化项目

---
+++ base/base_analyze.py
@@ -0,0 +1,14 @@
+import yaml
+
+
+def analyze_with_file(file_name, case_name):
+    with open("./data/" + file_name + ".yml", "r", encoding='utf-8') as f:
+        res = yaml.load(f)[case_name]
+
+        temp_list = list()
+        for values in res.values():
+            temp_list.append(values)
+
+        return temp_list
+
+
