bc94cd343d8a45df419a0dcb78fc49cabea2cdfb
DESKTOP-PJQETOT\14326 <1119367693@qq.com>
2018-08-25 11:02:31 +0800

    完善登录功能

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -1,5 +1,9 @@
+import pytest
+
+from base.base_analyze import analyze_with_file
 from base.base_driver import init_driver
 from page.page import Page
+from base.base_analyze import analyze_with_file
 
 
 class TestLogin:
@@ -7,9 +11,12 @@ class TestLogin:
         self.driver = init_driver()
         self.page = Page(self.driver)
 
-    def test_login1(self):
+    @pytest.mark.parametrize("args", analyze_with_file("login_data", "test_login1"))
+    def test_login1(self, args):
+        name = args["name"]
+        password = args["password"]
         self.page.home_button.click_mine()
         self.page.mine_button.click_login()
-        self.page.login_button.input_name("13800138006")
-        self.page.login_button.input_password("123456")
+        self.page.login_button.input_name(name)
+        self.page.login_button.input_password(password)
         self.page.login_button.click_login_button()
